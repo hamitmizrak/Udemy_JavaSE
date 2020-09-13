fb7486ad79ef831e8716300a770ae08ec1d575f2
Hamit Mizrak <hamitmizrak@gmail.com>
2020-09-13 15:25:14 +0300

    Instance Of örneği

---
+++ src/main/java/com/hamit/instanceOf/InstanceOf44.java
@@ -0,0 +1,13 @@
+package com.hamit.instanceOf;
+
+public class InstanceOf44 {
+
+	public static void main(String[] args) {
+		InstanceOf44 ins = new InstanceOf44();
+
+		if (ins instanceof InstanceOf44) {
+			System.out.println("Evet bir örneğidir");
+		}
+
+	}
+}
